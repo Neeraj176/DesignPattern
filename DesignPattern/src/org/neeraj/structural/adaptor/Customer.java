e1c9a24556b67d3edc670f00d58569978508b410
Neeraj Kumar <neerajkamra76@gmail.com>
2021-04-09 13:50:43 +0530

    Structural Design Pattern - Adaptor Design Pattern

---
+++ DesignPattern/src/org/neeraj/structural/adaptor/Customer.java
@@ -0,0 +1,14 @@
+package org.neeraj.structural.adaptor;
+
+/**
+ * Target interface required by new client code
+ * Target Interface
+ */
+public interface Customer {
+
+	String getName();
+	
+	String getDesignation();
+	
+	String getAddress();
+}
