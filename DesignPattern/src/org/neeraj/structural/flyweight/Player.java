d38fbdce58be25a7f7deafe886e87327dc22e4ca
Neeraj Kumar <neerajkamra76@gmail.com>
2021-04-22 11:24:16 +0530

    Structural Design Pattern - FlyWeight Design Pattern

---
+++ DesignPattern/src/org/neeraj/structural/flyweight/Player.java
@@ -0,0 +1,13 @@
+package org.neeraj.structural.flyweight;
+
+//A Java program to demonstrate working of
+//FlyWeight Pattern with example of Counter
+//Strike Game
+
+//A common interface for all players
+interface Player{
+
+	public void assignWeapon(String weapon);
+	public void mission();
+
+}
