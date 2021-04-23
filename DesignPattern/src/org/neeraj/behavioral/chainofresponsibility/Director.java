c964051fb31d09f974215eeef540a7b973f09900
Neeraj Kumar <neerajkamra76@gmail.com>
2021-04-23 11:23:22 +0530

    Behavioral Design Pattern - Chain of Responsibility

---
+++ DesignPattern/src/org/neeraj/behavioral/chainofresponsibility/Director.java
@@ -0,0 +1,21 @@
+package org.neeraj.behavioral.chainofresponsibility;
+
+import org.neeraj.behavioral.chainofresponsibility.LeaveApplication.Type;
+
+//A concrete handler
+public class Director extends Employee {
+
+	public Director(LeaveApprover nextApprover) {
+		super("Director", nextApprover);
+	}
+	
+	@Override
+	protected boolean processRequest(LeaveApplication application) {
+		if(application.getType() == Type.PTO) {
+			application.approve(getApproverRole());
+			return true;
+		}
+		return false;
+	}
+	
+}
